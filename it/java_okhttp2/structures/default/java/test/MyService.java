17ede89c59e14c126d097d9274b4f565909744fc
John-John Tedro <udoprog@tedro.se>
2018-03-20 11:55:32 +0100

    okhttp: more work

--- it/java_okhttp2/structures/default/java/test/MyService.java
+++ it/java_okhttp2/structures/default/java/test/MyService.java
@@ -2,9 +2,12 @@ package test;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public interface MyService {
   /**
@@ -37,11 +40,11 @@ public interface MyService {
 
   public class OkHttp implements MyService {
     private final OkHttpClient client;
-    private final Optional<String> baseUrl;
+    private final HttpUrl baseUrl;
 
     public OkHttp(
       final OkHttpClient client,
-      final Optional<String> baseUrl
+      final HttpUrl baseUrl
     ) {
       this.client = client;
       this.baseUrl = baseUrl;
@@ -49,55 +52,111 @@ public interface MyService {
 
     @Override
     public CompletableFuture<Void> unknown(final int id) {
-      final HttpUrl url = new HttpUrl.Builder()
+      final HttpUrl url = this.baseUrl.newBuilder()
         .addPathSegment("unknown")
         .addPathSegment(Integer.toString(id))
         .build();
-      new Request.Builder()
+      final Request request = new Request.Builder()
         .url(url)
         .build();
+      client.newCall(request).enqueue(new Callback{
+        @Override
+        public void onFailure(final Call call, final Call e) {
+          future.fail(e);
+        }
+
+        @Override
+        public void onResponse(final Call call, final Response response) {
+          if (!response.isSuccessful()) {
+            future.fail(new IOException("bad response: " + response))
+            return;
+          }
+        }
+      });
       throw new IllegalStateException("not implemented");
     }
 
     @Override
     public CompletableFuture<Entry> unknownReturn(final int id) {
-      final HttpUrl url = new HttpUrl.Builder()
+      final HttpUrl url = this.baseUrl.newBuilder()
         .addPathSegment("unknown-return")
         .addPathSegment(Integer.toString(id))
         .build();
-      new Request.Builder()
+      final Request request = new Request.Builder()
         .url(url)
         .build();
+      client.newCall(request).enqueue(new Callback{
+        @Override
+        public void onFailure(final Call call, final Call e) {
+          future.fail(e);
+        }
+
+        @Override
+        public void onResponse(final Call call, final Response response) {
+          if (!response.isSuccessful()) {
+            future.fail(new IOException("bad response: " + response))
+            return;
+          }
+        }
+      });
       throw new IllegalStateException("not implemented");
     }
 
     @Override
     public CompletableFuture<Void> unknownArgument(final Entry request, final int id) {
-      final HttpUrl url = new HttpUrl.Builder()
+      final HttpUrl url = this.baseUrl.newBuilder()
         .addPathSegment("unknown-argument")
         .addPathSegment(Integer.toString(id))
         .build();
-      new Request.Builder()
+      final Request request = new Request.Builder()
         .url(url)
         .build();
+      client.newCall(request).enqueue(new Callback{
+        @Override
+        public void onFailure(final Call call, final Call e) {
+          future.fail(e);
+        }
+
+        @Override
+        public void onResponse(final Call call, final Response response) {
+          if (!response.isSuccessful()) {
+            future.fail(new IOException("bad response: " + response))
+            return;
+          }
+        }
+      });
       throw new IllegalStateException("not implemented");
     }
 
     @Override
     public CompletableFuture<Entry> unary(final Entry request, final int id) {
-      final HttpUrl url = new HttpUrl.Builder()
+      final HttpUrl url = this.baseUrl.newBuilder()
         .addPathSegment("foo")
         .addPathSegment(Integer.toString(id))
         .build();
-      new Request.Builder()
+      final Request request = new Request.Builder()
         .url(url)
         .build();
+      client.newCall(request).enqueue(new Callback{
+        @Override
+        public void onFailure(final Call call, final Call e) {
+          future.fail(e);
+        }
+
+        @Override
+        public void onResponse(final Call call, final Response response) {
+          if (!response.isSuccessful()) {
+            future.fail(new IOException("bad response: " + response))
+            return;
+          }
+        }
+      });
       throw new IllegalStateException("not implemented");
     }
   }
 
   public static class OkHttpBuilder {
-    private Optional<String> baseUrl = Optional.empty();
+    private Optional<HttpUrl> baseUrl = Optional.empty();
     private final OkHttpClient client;
 
     public OkHttpBuilder(
@@ -106,13 +165,14 @@ public interface MyService {
       this.client = client;
     }
 
-    public OkHttpBuilder baseUrl(final String baseUrl) {
+    public OkHttpBuilder baseUrl(final HttpUrl baseUrl) {
       this.baseUrl = Optional.of(baseUrl);
       return this;
     }
 
     public OkHttp build() {
-      return new OkHttp(client, this.baseUrl);
+      final HttpUrl baseUrl = this.baseUrl.orElseThrow(() -> new RuntimeException("baseUrl: is a required field"));
+      return new OkHttp(client, baseUrl);
     }
   }
 }
